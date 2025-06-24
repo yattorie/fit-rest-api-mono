feee192e794427c93267d36f2a1ee94b6411ea91
yattorie <pyryryqw882@gmail.com>
2025-06-24 03:16:07 +0300

    Initial commit

---
+++ src/main/java/com/orlovandrei/fit_rest/FitRestApplication.java
@@ -0,0 +1,13 @@
+package com.orlovandrei.fit_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/orlovandrei/fit_rest/FitRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orlovandrei.fit_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
