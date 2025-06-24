e981b57e5940559b2d7a37849f5ddb9352d01612
yattorie <pyryryqw882@gmail.com>
2025-06-25 02:28:27 +0300

    feat(i18n): expand enum Messages

--- src/main/java/com/orlovandrei/fit_rest/util/Messages.java
+++ src/main/java/com/orlovandrei/fit_rest/util/Messages.java
@@ -10,14 +10,30 @@ public enum Messages {
     INVALID_REFRESH_TOKEN("Invalid refresh token"),
     IS_ALREADY_TAKEN(" is already taken."),
     USERNAME("Username "),
-    RECIPE_NOT_FOUND("Recipe not found."),
-    ARTICLE_NOT_FOUND("Article not found"),
+    RECIPE_NOT_FOUND_BY_ID("Recipe not found with id: "),
+    ARTICLE_NOT_FOUND_BY_ID("Article not found with id: "),
     IMAGE_UPLOAD_FAILED("Image upload failed"),
     MINIO_BUCKET("MinIO bucket '"),
     MINIO_BUCKET_ERROR_INIT("Error during MinIO bucket initialization."),
     CREATED("' created."),
     EXISTS("' already exists."),
-    USER_NOT_FOUND("User not found");
+    CATEGORY_NOT_FOUND_BY_ID("Category not found with id: "),
+    EDIT_ONLY_YOUR_COMMENT("You can only edit your own comment"),
+    DELETE_ONLY_YOUR_COMMENT("You can only delete your own comment"),
+    WORKOUT_NOT_FOUND_BY_ID("Workout not found with id: "),
+    GOAL_NOT_FOUND_BY_ID("Goal not found with id: "),
+    COMMENT_NOT_FOUND_BY_ID("Comment not found with id: "),
+    USER_NOT_FOUND_EMAIL("User not found with email: "),
+    USER_NOT_FOUND_BY_ID("User not found with id:"),
+    USER_NOT_FOUND_USERNAME("User not found with username: "),
+    MEAL_PLAN_NOT_FOUND_BY_ID("Meal Plan not found with id: "),
+    CHANGE_ONLY_YOUR_GOALS("You can only change your goals."),
+    GET_ONLY_YOUR_GOALS("You can only get your goals."),
+    DELETE_ONLY_YOUR_GOALS("You can only get your goals."),
+    USER_NOT_FOUND("User not found"),
+    PROFILE_INCOMPLETE("Profile data is incomplete. Please ensure weight, height, birthdate, and gender are provided."),
+    MEAL_PLAN_ACCESS_DENIED("You can only manage your own meal plans"),
+    MEAL_PLAN_OPERATION_DENIED("You can only manage your own meal plans");
 
     private final String message;
 }
