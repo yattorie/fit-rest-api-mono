d770acd3db780b9135b558587bb5a2d69a56e07e
yattorie <pyryryqw882@gmail.com>
2025-06-25 17:57:22 +0300

    feat: enable caching and transaction management

--- src/main/java/com/orlovandrei/fit_rest/FitRestApplication.java
+++ src/main/java/com/orlovandrei/fit_rest/FitRestApplication.java
@@ -2,8 +2,12 @@ package com.orlovandrei.fit_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableCaching
+@EnableTransactionManagement
 public class FitRestApplication {
 
     public static void main(String[] args) {
